96d89badfe562c2645c3bcd61009bddfd9109e73
= <hellospeakman@gmail.com>
2021-08-07 23:51:56 +0100

    Just familiarising myself with the discord events.

--- main.py
+++ main.py
@@ -1,15 +1,38 @@
+# bot.py
 import os
 
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv('BOT_TOKEN')
+TOKEN = os.getenv('DISCORD_TOKEN')
+LISTENING_CHANNEL = os.getenv('DISCORD_LISTEN_CHANNEL')
 
-client = discord.Client()
+class AnnounceClient(discord.Client):
+    """
+    A class used to announce
 
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    ...
 
+    Attributes
+    ----------
+    discord.Client : Client
+        The discord client to interact with
+
+    Methods
+    -------
+    on_ready(self)
+        Executed when the client is ready
+    """
+
+    async def on_ready(self):
+        print(f'{self.user} has connected to Discord!')
+
+    async def on_message(self, message):
+        if message.channel.id == LISTENING_CHANNEL:
+            await message.channel.send('Identified a message in #test-announce!')
+
+
+client = AnnounceClient()
 client.run(TOKEN)
